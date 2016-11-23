7ddbe07a9dbbcf4f1fd00115206fb5b391bf40f9
Jeffrey Haselby <jeffrey.haselby@gmail.com>
2016-11-23 13:37:04 -0700

    Added a text box, button, outlets and an action

--- MyFirstMacApplication/ViewController.swift
+++ MyFirstMacApplication/ViewController.swift
@@ -10,10 +10,16 @@ import Cocoa
 
 class ViewController: NSViewController {
 
+    @IBOutlet weak var label1: NSTextField!
+    
+    @IBOutlet weak var button1: NSButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        label1.stringValue = "Hello World"
+        button1.title = "Change Text"
     }
 
     override var representedObject: Any? {
@@ -22,6 +28,9 @@ class ViewController: NSViewController {
         }
     }
 
+    @IBAction func changetext(_ sender: Any) {
+        label1.stringValue = "New Text Created By Button Action"
+    }
 
 }
 
