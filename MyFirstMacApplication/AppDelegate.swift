59013a1444e0e40986a24a9b33271479b125f108
Jeffrey Haselby <jeffrey.haselby@gmail.com>
2016-11-23 12:48:15 -0700

    Initial Commit

---
+++ MyFirstMacApplication/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  MyFirstMacApplication
+//
+//  Created by Jeffrey Haselby on 11/23/16.
+//  Copyright © 2016 haselnet. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ MyFirstMacApplication/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  MyFirstMacApplication
+//
+//  Created by Jeffrey Haselby on 11/23/16.
+//  Copyright © 2016 haselnet. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
